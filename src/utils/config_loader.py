4a96cd71f46ae7f60adf1405863a4a3ba88093f9
Herencia López-Menchero, Andrés <aherencia@minsait.com>
2024-12-09 18:36:23 +0100

    add: docker support

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -21,6 +21,7 @@ def load_config(config_path: str = "./src/config/config.yaml") -> dict:
     try:
         with open(config_path, "r", encoding="utf-8") as file:
             config = yaml.safe_load(file)
-            return config
+            return config   
+
     except yaml.YAMLError as e:
         raise RuntimeError(f"Error parsing configuration file: {e}")
